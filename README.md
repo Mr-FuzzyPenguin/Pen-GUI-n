e9ee978396f2aa76aa12b49cb047b612ed52209b
penguin <penguin@archlinux>
2024-10-02 15:21:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pen-GUI-n
+I like to use Xournal++, but I wish there was a way to customize hotkeys for pen(s), eraser(s), and higlighter(s). This plugin adds that functionality. It is also my first (of many) Lua projects.
---
+++ main.lua
@@ -0,0 +1,65 @@
+function initUi()
+    app.registerUi({
+        ["menu"] = "Pen tool",
+        ["callback"] = "pen",
+        ["accelerator"] = "p"
+    });
+    app.registerUi({
+        ["menu"] = "Eraser tool",
+        ["callback"] = "eraser",
+        ["accelerator"] = "e"
+    });
+    app.registerUi({
+        ["menu"] = "Eraser tool",
+        ["callback"] = "text",
+        ["accelerator"] = "t"
+    });
+    app.registerUi({
+        ["menu"] = "Rectangular Select tool",
+        ["callback"] = "rect_sel",
+        ["accelerator"] = "r"
+    });
+    app.registerUi({
+        ["menu"] = "\"Squiggly\" Select tool",
+        ["callback"] = "squig_sel",
+        ["accelerator"] = "s"
+    });
+    app.registerUi({
+        ["menu"] = "Highlighter tool",
+        ["callback"] = "highlighter",
+        ["accelerator"] = "v"
+    });
+    app.registerUi({
+        ["menu"] = "LaTex tool",
+        ["callback"] = "latex",
+        ["accelerator"] = "x"
+    });
+end
+
+function pen()
+    app.uiAction({ ["action"] = "ACTION_TOOL_PEN" })
+end
+
+function eraser()
+    app.uiAction({ ["action"] = "ACTION_TOOL_ERASER" })
+end
+
+function text()
+    app.uiAction({ ["action"] = "ACTION_TOOL_TEXT" })
+end
+
+function rect_sel()
+    app.uiAction({ ["action"] = "ACTION_TOOL_SELECT_RECT" })
+end
+
+function squig_sel()
+    app.uiAction({ ["action"] = "ACTION_TOOL_SELECT_REGION" })
+end
+
+function highlighter()
+    app.uiAction({ ["action"] = "ACTION_TOOL_HIGHLIGHTER" })
+end
+
+function latex()
+    app.uiAction({ ["action"] = "ACTION_TEX" })
+end
